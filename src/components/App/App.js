1ab3f0a189235c1c237faf7237b7b35841c60e95
AirDNA6 <Key3erS0ze@protonmail.com>
2020-09-29 17:50:23 +0200

    updated

---
+++ src/components/App/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Header from '../elements/Header/Header'
+import Footer from '../elements/Footer/Footer'
+import Home from '../Home/Home'
+import About from '../About/About'
+import Halls from '../Halls/HallInfo'
+import Contact from '../Contact/Contact'
+import Movie from '../Movie/Movie'
+import NotFound from '../elements/NotFound/NotFound'
+import ScrollToTop from '../ScrollToTop'
+import Scroll from '../Scroll'
+import Faq from '../Faq'
+
+
+
+
+
+
+
+
+const App = () => {
+
+
+  return (
+    <div>
+     <BrowserRouter>
+     <ScrollToTop />
+      <React.Fragment>
+
+      <Header />
+      <Scroll showBelow={250} />
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/halls" component={Halls} exact />
+          <Route path="/about" component={About} exact />
+          <Route path="/contact" component={Contact} exact />
+          <Route path="/faq" component={Faq} exact />
+          <Route path="/:movieId"  component={Movie} exact />
+
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    </BrowserRouter>
+    </div>
+    
+  )
+}
+
+export default App
--- src/components/Halls/HallInfo.js
+++ src/components/Halls/HallInfo.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -7,24 +6,11 @@ import "mdbreact/dist/css/mdb.css";
 import Image from "react-bootstrap/Image";
 import { Container, Row, Col } from "react-bootstrap";
 import "./HallInfo.css";
-=======
-import React, { Component } from 'react'
-
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import './HallInfo.css'
-
-
-
->>>>>>> d058085e81dc8298a817ccdc7dfa0ad2b4e4eebb
 
 class About extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
         <Container fluid className="aboutBack mb-5 mt-5">
           <Row className="aboutBack">
             <h1 className="mx-auto">Hall 1</h1>
@@ -173,166 +159,3 @@ class About extends Component {
 }
 
 export default About;
-=======
-        <Container className="aboutBack mt-5">
-
-          <Row className="aboutBack">
-            <h1 className="mx-auto">Hall 1</h1>
-          </Row>
-
-
-          <Row className="aboutBack mb-5 mt-5 ">
-            <Col sm={12} xs={12} md={12} lg={6} className="center">
-              <img src="../images/sala1.jpg" alt="sala1" className="img-fluid mb-5" style={{
-               width: '100%',
-              }}
-              />
-            </Col>
-
-            <Col sm={12} xs={12} md={12} lg={6} >
-              <img src="../images/planSala1.png" alt="planSala1" className="img-fluid mb-5" style={{
-                width: '100%',
-              }}/>
-            </Col>
-            <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reprehenderit distinctio pariatur amet id harum perferendis, numquam cumque corporis, non architecto dolorem, enim ab voluptate aliquam veniam asperiores deleniti dolores.</p>
-          </Row>
-        </Container>
-
-
-        <Container className="aboutBack">
-
-<Row className="aboutBack">
-  <h1 className="mx-auto">Hall 2</h1>
-</Row>
-
-
-<Row className="aboutBack mb-5 mt-5 ">
-  <Col sm={12} xs={12} md={12} lg={6} className="center">
-    <img src="../images/sala2.jpg" alt="sala2" className="img-fluid mb-5" style={{
-     width: '100%',
-    }}
-    />
-  </Col>
-
-  <Col sm={12} xs={12} md={12} lg={6} >
-    <img src="../images/planSala2.png" alt="planSala2" className="img-fluid mb-5" style={{
-      width: '100%',
-    }}/>
-  </Col>
-  <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reprehenderit distinctio pariatur amet id harum perferendis, numquam cumque corporis, non architecto dolorem, enim ab voluptate aliquam veniam asperiores deleniti dolores.</p>
-</Row>
-</Container>
-
-
-<Container className="aboutBack">
-
-<Row className="aboutBack">
-  <h1 className="mx-auto">Hall 3</h1>
-</Row>
-
-
-<Row className="aboutBack mb-5 mt-5 ">
-  <Col sm={12} xs={12} md={12} lg={6} className="center">
-    <img src="../images/sala3.jpg" alt="sala3" className="img-fluid mb-5" style={{
-     width: '100%',
-    }}
-    />
-  </Col>
-
-  <Col sm={12} xs={12} md={12} lg={6} >
-    <img src="../images/planSala3.png" alt="planSala3" className="img-fluid mb-5" style={{
-      width: '100%',
-      height: '89%',
-    }}/>
-  </Col>
-  <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reprehenderit distinctio pariatur amet id harum perferendis, numquam cumque corporis, non architecto dolorem, enim ab voluptate aliquam veniam asperiores deleniti dolores.</p>
-</Row>
-</Container>
-
-
-<Container className="aboutBack">
-
-<Row className="aboutBack">
-  <h1 className="mx-auto">Hall 4</h1>
-</Row>
-
-
-<Row className="aboutBack mb-5 mt-5 ">
-  <Col sm={12} xs={12} md={12} lg={6} className="center">
-    <img src="../images/sala4.jpg" alt="sala4"className="img-fluid mb-5" style={{
-     width: '100%',
-    }}
-    />
-  </Col>
-
-  <Col sm={12} xs={12} md={12} lg={6} >
-    <img src="../images/planSala4.png" alt="planSala4"className="img-fluid mb-5" style={{
-      width: '100%',
-    }}/>
-  </Col>
-  <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reprehenderit distinctio pariatur amet id harum perferendis, numquam cumque corporis, non architecto dolorem, enim ab voluptate aliquam veniam asperiores deleniti dolores.</p>
-</Row>
-</Container>
-
-
-<Container className="aboutBack">
-
-<Row className="aboutBack">
-  <h1 className="mx-auto">Hall 5</h1>
-</Row>
-
-
-<Row className="aboutBack mb-5 mt-5 ">
-  <Col sm={12} xs={12} md={12} lg={6} className="center">
-    <img src="../images/sala5.jpg" alt="sala5" className="img-fluid mb-5" style={{
-     width: '100%',
-    }}
-    />
-  </Col>
-
-  <Col sm={12} xs={12} md={12} lg={6} >
-    <img src="../images/planSala5.png"  alt="planSala5" className="img-fluid mb-5" style={{
-      width: '100%',
-    }}/>
-  </Col>
-  <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reprehenderit distinctio pariatur amet id harum perferendis, numquam cumque corporis, non architecto dolorem, enim ab voluptate aliquam veniam asperiores deleniti dolores.</p>
-</Row>
-</Container>
-
-
-<Container className="aboutBack">
-
-<Row className="aboutBack">
-  <h1 className="mx-auto">Hall 6</h1>
-</Row>
-
-
-<Row className="aboutBack mb-5 mt-5 ">
-  <Col sm={12} xs={12} md={12} lg={6} className="center">
-    <img src="../images/sala6.jpg" alt="sala6"className="img-fluid mb-5" style={{
-     width: '100%',
-    }}
-    />
-  </Col>
-
-  <Col sm={12} xs={12} md={12} lg={6} >
-    <img src="../images/planSala6.png" alt="planSala6"className="img-fluid mb-5" style={{
-      width: '100%',
-      height: '89%'
-    }}/>
-  </Col>
-  <p className="p-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga reprehenderit distinctio pariatur amet id harum perferendis, numquam cumque corporis, non architecto dolorem, enim ab voluptate aliquam veniam asperiores deleniti dolores.</p>
-</Row>
-</Container>
-
-      </div>
-
-
-
-
-    )
-  }
-}
-
-export default About
->>>>>>> d058085e81dc8298a817ccdc7dfa0ad2b4e4eebb
