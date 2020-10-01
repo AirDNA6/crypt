c809bfc387e40b64363a1d025347513aac9f4035
AirDNA6 <Key3erS0ze@protonmail.com>
2020-10-01 13:10:58 +0200

    faq heading, secondary heading

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -16,12 +16,14 @@ const useStyles = makeStyles((theme) => ({
     fontSize: theme.typography.pxToRem(15),
     flexBasis: '100%',
     flexShrink: 0,
-    fontFamily: 'Quantico'
+    fontFamily: 'Quantico',
+    color: '#21d192'
   },
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
-    fontFamily: 'Quantico'
+    fontFamily: 'Quantico',
+    color: '#fff'
   },
 }));
 
@@ -36,8 +38,7 @@ export default function Faq() {
   return (
     <div className={classes.root} className="container mt-5 mb-5">
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -49,7 +50,7 @@ export default function Faq() {
           {/* <Typography className={classes.secondaryHeading}>I am an accordion</Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           Register for free on our website (until registration). The code you receive via e-mail you need to show at the box office, on the basis of which the employee issues you a Cineplexx bonus card. <br/><br />
 
 Already have an online account on our site? In that case, there is no need to re-register. All you have to do is pick up your Cineplexx bonus card based on your date of birth and e-mail address the next time you visit the box office. 
@@ -57,8 +58,7 @@ Already have an online account on our site? In that case, there is no need to re
         </AccordionDetails>
       </Accordion>
       <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -71,15 +71,14 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-          <Typography style={{fontFamily: 'Quantico'}}>
+          <Typography className={classes.secondaryHeading}>
           You can exchange your points at the box office for free tickets or items from the food and beverage counter. 
           </Typography>
         </AccordionDetails>
       </Accordion>
 
       <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -92,25 +91,24 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           You can check the status of your bonus points online at any time . You need to select your name in the upper right corner and check the "Crypt bonus card" option to see the status of the collected points. 
           </Typography>
         </AccordionDetails>
       </Accordion>
 
       <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
+          expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }}/>}
           aria-controls="panel4bh-content"
           id="panel4bh-header"
         >
           <Typography className={classes.heading}>What is the validity period of funds and points on my Crypt bonus card?</Typography>
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           The amount paid on the Crypt bonus card is valid for 18 months from the date of payment. After 18 months, the discounts associated with the Cineplexx bonus card are no longer valid. The remaining amount cannot be returned to the cardholder, but can be used as a non-cash means of ticket payment (without discount) for the next 60 months. After the expiration of those 5 years, the funds on the card cease to be valid. <br/><br/>
           Bonus points that you collect on the Crypt bonus card can be redeemed for a period of 18 months from the day they were collected. After 18 months, the points are deleted, however, points that are not older than 18 months remain on the card and can be used until they expire.<br/><br/>
           Funds on the card (money and points) cannot be redeemed, nor can they be transferred to another user's Cineplexx bonus card. 
@@ -120,8 +118,7 @@ Already have an online account on our site? In that case, there is no need to re
 
       
       <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -134,7 +131,7 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           Yes, we take credit and debit cards online, at the box office and in the Crypt Club.
           </Typography>
         </AccordionDetails>
@@ -142,8 +139,7 @@ Already have an online account on our site? In that case, there is no need to re
 
 
       <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -156,15 +152,14 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           We kindly ask that you refrain from bringing outside food and drink into the cinema. We have an extensive food and drinks menu for you to choose from.
           </Typography>
         </AccordionDetails>
       </Accordion>
 
       <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -177,15 +172,14 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           Our call centre is now back open, so you can call us on + 65 1327 172 or get in contact via email on customer.services@crypt.com
           </Typography>
         </AccordionDetails>
       </Accordion>
 
       <Accordion expanded={expanded === 'panel8'} onChange={handleChange('panel8')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -198,15 +192,14 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           Marvel night is the event dedicated to the release of superhero, action movies and movies realized by the comics of Marvel studio, when visitors can get double pains with a bonus card and various gifts.
           </Typography>
         </AccordionDetails>
       </Accordion>
 
       <Accordion expanded={expanded === 'panel9'} onChange={handleChange('panel9')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -219,7 +212,7 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           DC night is the event dedicated to the release of superhero, action movies and movies realized by the comics of DC studio, when visitors can get double pains with a bonus card and various gifts.
           </Typography>
         </AccordionDetails>
@@ -227,8 +220,7 @@ Already have an online account on our site? In that case, there is no need to re
 
 
       <Accordion expanded={expanded === 'panel10'} onChange={handleChange('panel10')} style={{
-        backgroundColor: '#343A40',
-        color: '#21d192'
+        backgroundColor: '#343A40'
       }}>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon style={{ color: '#21d192' }} />}
@@ -241,7 +233,7 @@ Already have an online account on our site? In that case, there is no need to re
           </Typography> */}
         </AccordionSummary>
         <AccordionDetails>
-        <Typography style={{fontFamily: 'Quantico'}}>
+        <Typography className={classes.secondaryHeading}>
           Please inform us about the loss or damage of the card by e-mail at bonuscard@crypt.com . You need to send us your Crypt bonus card number, which is why we advise you to keep that number separate from the card itself. After losing the Crypt bonus card, the owner will receive a new card after the given number of the lost card and the identification document. Funds and bonus points will be transferred from the old to the new Crypt bonus card, with the amount reduced by manipulative costs in the amount of 250 dinars. The new Crypt bonus card will be valid for the same period as the lost card. 
           </Typography>
         </AccordionDetails>
